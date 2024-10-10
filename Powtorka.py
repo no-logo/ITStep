40ae9e54fed37c025ccba3b07ddfb6493e936f62
no_logo <andrzej.wawryk@gmail.com>
2024-10-10 18:58:38 +0200

    plik powtórka

---
+++ Powtorka.py
@@ -0,0 +1,28 @@
+print('')
+
+x = y = z = 'orange'
+print(x)
+print(y)
+print(z)
+
+print('')
+
+fruits = ["apple", "banana", "chery"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+print('')
+
+fruits = ["apple", "banana", "chery"]
+x, *z = fruits
+print(x)
+print(z)
+
+print('')
+
+d = {'a'  : 1, 'b' : 2, 'c' : 3}
+a1, *a = d
+print(a1)
+print(a)
