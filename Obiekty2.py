ead4b055577758c7338988c2d2e50dac7d4ef0e3
no_logo <andrzej.wawryk@gmail.com>
2024-10-18 19:38:15 +0200

    clasa iterator

--- Obiekty2.py
+++ Obiekty2.py
@@ -34,4 +34,85 @@ car, boat, plane = Car('Cadilac'), Boat('Elan'), Plane('Cesna')
 
 for x in (car, boat, plane):
     x.move()                    #POLIMORFIZM X WYOŁUJE JEDNĄ METODĘ I JEST WOŁANA ODPOWIEDNIA DLA KAŻDEJ KLASY
+
+class Vehicle:
+    def __init__(self, name, speed) -> None:
+        self.name = name
+        self.speed = speed
+
+    def move(self):
+        print('Vehicle is moving')
+
+class Car(Vehicle):
+    pass
+
+c = Car('Car', 50)
+c.move()
+
+class Car2(Vehicle):
+    def move(self):
+        print('Drive')
+
+c2 = Car2('Car', 50)
+c2.move()
+
+l = ['a', 'b', 'c', 'd']
+it = iter(l)
+print(next(it), next(it), next(it))
+
+s = 'abscdlkan'
+itr = iter(s)
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+
+#klasy z możliwoscią iteracji
+
+class MyIterable:
+    def __iter__(self):
+        self.a = 1
+        return self    # wazne retukrn
+
+    def __next__(self):
+        x = self.a
+        self.a += 3
+        return x        # wazne retukrn
     
+mi = MyIterable()
+it = iter(mi)
+
+print(next(it), next(it), next(it))
+
+class MyRange:
+    def __init__(self, start = 0, stop = 0, step = 1) -> None:
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self):
+        self.current = self.start
+        return self
+    
+    def __next__(self):
+        x = self.current
+        
+        if self.current >= self.stop:
+            raise StopIteration
+        self.current += self.step
+        return x
+    
+r = MyRange(start = 0, stop = 10, step = 1)
+it = iter(r)
+
+for x in r:
+    print(x)
+
+
+
+it = iter(r)
+
+for x in r:
+    print(x)
+        
+        
