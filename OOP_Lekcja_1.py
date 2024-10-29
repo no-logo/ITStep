35ada794909276918c333b8a51605000d8abe0dc
no-logo <andrzej.wawryk@gmail.com>
2024-10-29 19:31:12 +0100

    Human get set get

--- OOP_Lekcja_1.py
+++ OOP_Lekcja_1.py
@@ -22,9 +22,42 @@ class Human():
         return self.__coutry
     def get_address(self):
         return self.__address
+    
+    def set_first_name(self, first_name):
+        self.__first_name = first_name
+    def set_last_name(self, last_name):
+        self.__last_name = last_name
+    def set_date_of_birth(self, date_of_birth):
+        self.__date_of_birth = date_of_birth
+    def set_contact_number(self, contact_number):
+        self.__contact_number = contact_number
+    def set_city(self, city):
+        self.__city = city
+    def set_country(self, coutry):
+        self.__coutry = coutry
+    def set_address(self, address):
+        self.__address = address
 
 
 human = Human('Andrzej', 'Wawryk', '1975-01-03', '536956536', 'Kraków', 'Polska', 'Ulica 1/23 12-345 Kraków')
+
+print(human.get_first_name())
+print(human.get_last_name())
+print(human.get_date_of_birth())
+print(human.get_contact_number())
+print(human.get_city())
+print(human.get_country())
+print(human.get_address())
+
+human.set_first_name('aaa')
+human.set_last_name('bbb')
+human.set_date_of_birth('2005-05-10')
+human.set_contact_number('123456789')
+human.set_city('Wroclaw')
+human.set_country('POLSKA')
+human.set_address('aaabbb 123 12-123 Wroclaw')
+
+print('')
 print(human.get_first_name())
 print(human.get_last_name())
 print(human.get_date_of_birth())
