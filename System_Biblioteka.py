ea933631214c34e6925705f216fedf665bf6784b
no-logo <andrzej.wawryk@gmail.com>
2024-10-31 20:39:11 +0100

    books

--- System_Biblioteka.py
+++ System_Biblioteka.py
@@ -26,3 +26,54 @@ class Book:
     def is_available(self):
         return self.__available
     
+
+class Person:
+    def __init__(self, name, id_number) -> None:
+        self.__name = name
+        self.__id_number = id_number
+
+    def get_info(self):
+        return f'name: {self.__name}, id number: {self.__id_number}'
+    
+
+class Reader(Person):
+    def __init__(self, name, id_number) -> None:
+        super().__init__(name, id_number)
+        self.__borrowed_books = []
+
+    def borrow_book(self, book: Book):
+        if book.is_available():
+            self.__borrowed_books.append(book)
+            book.borrow()
+            print(f'book {book.get_title()} borrowed')
+        else:
+            print(f'{book.get_title()} is not available')
+
+    def return_book(self, book: Book):
+        self.__borrowed_books.remove(book)
+        book.return_book()
+
+
+class Librarian(Person):
+    def __init__(self, name, id_number) -> None:
+        super().__init__(name, id_number)
+
+    @staticmethod
+    def check_availability(book: Book):
+        return book.is_available()
+    
+b1 = Book('title b1', 'autnor b1', 'b1 123456', True)
+b2 = Book('title b2', 'autnor b2', 'b2 123456', True)
+l = Librarian('Librarian name', '123456')
+r = Reader('Reader name', 123456)
+
+print(l.check_availability(b1))
+print(l.check_availability(b2))
+
+r.borrow_book(b1)
+r.borrow_book(b1)
+
+print(l.check_availability(b1))
+
+r.return_book(b1)
+print(l.check_availability(b1))
