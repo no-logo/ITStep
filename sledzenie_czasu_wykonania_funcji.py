67c540a58e6946f1fadb46786de384d9d37fbea6
no-logo <andrzej.wawryk@gmail.com>
2024-11-05 20:39:58 +0100

    miernik wykonania

---
+++ sledzenie_czasu_wykonania_funcji.py
@@ -0,0 +1,20 @@
+import time
+
+def fun_wait():
+    time.sleep(5)
+
+def miernik_wykonania(f):
+    licznik = 0
+    def zmierz_czas():
+        nonlocal licznik
+        licznik += 1
+        t = time.time()
+        f()
+        return [time.time() - t, licznik]
+    return zmierz_czas
+        
+
+miernik = miernik_wykonania()    
+
+print(miernik())
+    
