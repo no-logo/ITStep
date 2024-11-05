7c97ceb7312a92f703608913f408f371e317ff10
no-logo <andrzej.wawryk@gmail.com>
2024-11-05 20:22:40 +0100

    system rabatowy

---
+++ System_Rabatowy_W_Sklepie.py
@@ -0,0 +1,15 @@
+def stworz_rabat(rabat):
+    def cena_po_rabacie(cena):
+        nonlocal rabat
+        return cena - (cena * rabat / 100)
+    return cena_po_rabacie
+
+black_friday_rabat = stworz_rabat(20)
+cybar_monday_rabat = stworz_rabat(15)
+
+print(black_friday_rabat(100))
+print(cybar_monday_rabat(200))
+print(black_friday_rabat(50))
+
+
+
