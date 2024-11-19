59d5e5fe23dda793b117f88a1cd8a4ad78cf6e5a
no-logo <andrzej.wawryk@gmail.com>
2024-11-19 20:32:16 +0100

    auto constructor

--- AutomatyczneTworzenieKonstruktora.py
+++ AutomatyczneTworzenieKonstruktora.py
@@ -4,7 +4,7 @@ class ConstuctorMeta(type):
 
         def __init__(self, **kwargs):
             for attr_name, attr_type in attributes.items():
-                if attr_name in kwargs[attr_name]:
+                if attr_name in kwargs:
                     value = kwargs[attr_name]
                     if attr_type and not isinstance(value, attr_type):
                         raise TypeError('type error')
@@ -18,5 +18,14 @@ class ConstuctorMeta(type):
 
         return super().__new__(cls, name, bases, dct)
     
-class Person(metaclass = ConstuctorMeta):
+class ConstructorClass(metaclass = ConstuctorMeta):
+    pass
+    
+class Person(ConstructorClass):
     __atributes__ = {"name" : str, "age"  : int}
+
+p1 = Person(name = 'Alice', age = 30)
+print(p1.name, p1.age)
+
+p2 = Person(name = 'Alice2')
+print(p2.name, p2.age)
