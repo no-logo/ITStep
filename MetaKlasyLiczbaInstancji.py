f226d9c75ac83240846069d95e6fc1a30cf18f2f
no-logo <andrzej.wawryk@gmail.com>
2024-11-19 19:52:26 +0100

    zliczanie obiektów

--- MetaKlasyLiczbaInstancji.py
+++ MetaKlasyLiczbaInstancji.py
@@ -2,22 +2,30 @@
 
 
 class MetaClass(type):
-    _num_of_instances = 0
     def __new__(cls, name, bases, dct):
         dct['_num_of_instances'] = 0
-        dct['get_instance_count'] = classmethod(lambda self: self._num_of_instances)
-        return super().__new__(cls,name,bases,dct)
+        dct['get_instance_count'] = classmethod(lambda cls: cls._num_of_instances)
+        print(type(super().__new__(cls,name,bases,dct)))
+        return super().__new__(cls,name,bases,dct) # zwraca instancje metaklasy
     
     def __call__(cls, *args, **kwargs):
         cls._num_of_instances +=1
-        return super().__call__(*args, **kwargs)
+        print(type(super().__call__(*args, **kwargs)))
+        return super().__call__(*args, **kwargs) # zwraca instancje klasy
 
+print('=============')
 
 class MyClass(metaclass = MetaClass):
     pass
 
-obj1 = MyClass()
-obj2 = MyClass()     
-obj3 = MyClass()  
+print('------------')
 
+obj1 = MyClass()
 print(MyClass.get_instance_count())
+obj2 = MyClass()     
+print(MyClass.get_instance_count())
+obj3 = MyClass()  
+print(MyClass.get_instance_count())
+
+print(type(obj1))
+print(obj2.__dict__)
