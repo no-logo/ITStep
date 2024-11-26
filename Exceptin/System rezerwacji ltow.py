3c9efa0568feb5f9ac243103421de2824e8181a0
no-logo <andrzej.wawryk@gmail.com>
2024-11-26 18:51:05 +0100

    system rezerwacj

--- Exceptin/System rezerwacji ltow.py
+++ Exceptin/System rezerwacji ltow.py
@@ -14,14 +14,41 @@ class Flight:
         self._reservations = reservations
 
     def flight_reservation(self, flight_number, nuber_of_places):
-        pass
+        av = self.check_avalibility(self, flight_number)
+        try:
+            if av >= nuber_of_places:
+                self._reservations += nuber_of_places
+            else:
+                raise FlightFullError(f'Only av places are available')
+        except FlightFullError as e:
+            print(e)
 
     def cancel_flight_reservations(self, flight_number, nuber_of_places):
-        pass
+        try:
+            if self._flight_number != flight_number:
+                raise FlightNotFoudError("Flight number not exists")
+            if self._reservations < nuber_of_places:
+                raise NoReservationError("Too much places to cancel")
+            else:
+                self._reservations - nuber_of_places
+                print("Reservations canceled")
+        except FlightNotFoudError as fnf:
+            print(fnf)
+        except NoReservationError as nre:
+            print(nre)
 
     def check_avalibility(self, flight_number):
         try:
             if flight_number != self._flight_number:
-                raise FlightNotFoudError
+                raise FlightNotFoudError("Flight number not exists")
+            else:
+                if self._capacity > self._reservations:
+                    return self._capacity - self._reservations
+                else:
+                    return 0
+        except FlightNotFoudError as e:
+            print(e)
+
+                
         
         
