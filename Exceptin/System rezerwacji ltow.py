dd6489f01cc5c464caf85685c03df81f0c416da2
no-logo <andrzej.wawryk@gmail.com>
2024-11-28 19:57:13 +0100

    logowanie rezerwacji miejsc

--- Exceptin/System rezerwacji ltow.py
+++ Exceptin/System rezerwacji ltow.py
@@ -1,3 +1,8 @@
+fileName = 'logs.txt'
+from datetime import date
+from datetime import datetime
+
+
 class FlightFullError(Exception):
     pass
 
@@ -25,6 +30,8 @@ class Flight:
                 else:
                     return x
         except FlightNotFoudError as e:
+            with open(fileName, 'a') as logfile:  
+                logfile.write(f'[{datetime.now()}] {e}\n')
             print(e)
         return x
 
@@ -37,6 +44,8 @@ class Flight:
             else:
                 raise FlightFullError(f'Only {av} places are available')
         except FlightFullError as e:
+            with open(fileName, 'a') as logfile:  
+                logfile.write(f'[{datetime.now()}] {e}\n')
             print(e)
 
     def cancel_flight_reservations(self, flight_number, nuber_of_places):
@@ -49,16 +58,32 @@ class Flight:
                 self._reservations -= nuber_of_places
                 print("Reservations canceled")
         except FlightNotFoudError as fnf:
+            with open(fileName, 'a') as logfile:  
+                logfile.write(f'[{datetime.now()}] {fnf}\n')
             print(fnf)
         except NoReservationError as nre:
+            with open(fileName, 'a') as logfile:  
+                logfile.write(f'[{datetime.now()}] {nre}\n')
             print(nre)
 
     
-                
-        
-flight = Flight(123,10,0)
-print(flight.check_avalibility(123))
-flight.flight_reservation(123,11)
-print(flight.check_avalibility(123))
-flight.cancel_flight_reservations(123,2)
-print(flight.check_avalibility(123))
+with open(fileName, 'w') as logfile:              
+    flight = Flight(123,10,0)
+    logfile.write(f'[{datetime.now()}] 1 ustawienie nr lotu {flight._flight_number} ilosci miejsc {flight._capacity} oraz rezerwacji {flight._reservations}\n')
+
+with open(fileName, 'a') as logfile:  
+    #print(flight.check_avalibility(123))
+    logfile.write(f'[{datetime.now()}] 2 sprawdzenie ilosci dostepnych miejsc {flight.check_avalibility(123)}\n')
+    #flight.flight_reservation(123,11)
+    logfile.write(f'[{datetime.now()}] 3 rezerwacja 11 miejsc {flight.flight_reservation(123,11)}\n')
+    #print(flight.check_avalibility(123))
+    logfile.write(f'[{datetime.now()}] 4 sprawdzenie ilosci dostepnych miejsc {flight.check_avalibility(123)}\n')
+    #flight.cancel_flight_reservations(123,2)
+    logfile.write(f'[{datetime.now()}] 5 rezerwacja 2 miejsc {flight.flight_reservation(123,2)}\n')
+    #print(flight.check_avalibility(123))
+    logfile.write(f'[{datetime.now()}] 6 sprawdzenie ilosci dostepnych miejsc {flight.check_avalibility(123)}\n')
+
+
+
+
+
