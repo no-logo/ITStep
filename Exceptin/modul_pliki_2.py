6a8fdf7a2f46991ec44bcb62aafb3f7f86a2320d
no-logo <andrzej.wawryk@gmail.com>
2024-12-03 19:59:49 +0100

    pliki modul 2

---
+++ Exceptin/modul_pliki_2.py
@@ -0,0 +1,4 @@
+with open(r'C:\Users\User\ITStep\Exceptin\modul_pliki_2_brzydkie_slowa.txt', 'r') as file_brzydkie_slowa:
+    lines = [i.replace('\n', '') for i in file_brzydkie_slowa.readlines()]
+
+   
