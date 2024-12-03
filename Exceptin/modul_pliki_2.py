678c06bd497456dcfacdda7f617443d2c485c3ed
no-logo <andrzej.wawryk@gmail.com>
2024-12-03 20:24:59 +0100

    brzydkie slowa

--- Exceptin/modul_pliki_2.py
+++ Exceptin/modul_pliki_2.py
@@ -1,4 +1,15 @@
 with open(r'C:\Users\User\ITStep\Exceptin\modul_pliki_2_brzydkie_slowa.txt', 'r') as file_brzydkie_slowa:
     lines = [i.replace('\n', '') for i in file_brzydkie_slowa.readlines()]
 
+with open(r'C:\Users\User\ITStep\Exceptin\modul_pliki_2_usun_brzydkie_slowa.txt', 'r+') as file1:
+    content = file1.read()
+    print(content)
+    for word in lines:
+        content = content.replace(word, '')
+
+    file1.truncate(0)
+    file1.seek(0)
+    file1.write(content)
+            
+
    
